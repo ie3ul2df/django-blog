0be3ab911781375384a44417a045dfe93e436deb
arash12javadi <ie3ul2df@students.codeinstitute.net>
2025-06-04 17:40:31 +0100

    hide SERCET_KEY
    I put the secret key in env.py file and called it like
    SECRET_KEY = os.environ.get("SECRET_KEY")
    on my settings.py.
    Also introduced a new key in my Heroku app setting as SECRET_KEY and added the secret key there.

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-plpkjbek%998j4*t*^k_)z_u%%bo!jo0e*mo5!oc(w982wy6*0'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
