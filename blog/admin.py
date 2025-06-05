f00c995aadd3dcd2310cc181f2224cb8c76b3424
arash12javadi <ie3ul2df@students.codeinstitute.net>
2025-06-05 19:03:13 +0100

    Admin panel powered up by django-summernote

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from .models import Post
-from .models import Comment  
+from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
-admin.site.register(Post)
-admin.site.register(Comment)  
+admin.site.register(Comment)  # ✅ Registered using default model admin
+# admin.site.register(Post)
+
+@admin.register(Post)         # ✅ Registered using Summernote-enhanced class
+class PostAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title']
+    list_filter = ('status', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}  # Slug auto-fills from title
+    summernote_fields = ('content',)            # Rich text for content
