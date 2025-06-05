e0b78a8683e5c52f83d9cb0433944052b144df8c
arash12javadi <ie3ul2df@students.codeinstitute.net>
2025-06-05 22:49:41 +0100

    created template and added CSS

--- blog/views.py
+++ blog/views.py
@@ -17,4 +17,6 @@ class PostList(generic.ListView):
     # queryset = Post.objects.all()
     # queryset = Post.objects.filter(author=1)
     queryset = Post.objects.filter(status=1)
-    template_name = "post_list.html"
+    # template_name = "post_list.html"
+    template_name = "blog/index.html"
+    paginate_by = 6
