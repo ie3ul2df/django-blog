b48406cefe34489e63693aab5a09494d97b89b40
arash12javadi <ie3ul2df@students.codeinstitute.net>
2025-06-06 12:54:04 +0100

    corrected urls for about page

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.hello_blog, name='hello_blog'),
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
--- codestar/urls.py
+++ codestar/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),  # For Summernote editor
-    path("", include("blog.urls"), name="blog-urls"),
     path("about/", include("about.urls")),
+    path("", include("blog.urls"), name="blog-urls"),
 ]
