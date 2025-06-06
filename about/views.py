e2033eee04b7111f80da726ab71a84ed90414c79
arash12javadi <ie3ul2df@students.codeinstitute.net>
2025-06-06 23:42:29 +0100

    refined codes

--- about/views.py
+++ about/views.py
@@ -3,18 +3,19 @@ from .models import About
 from .forms import CollaborateForm
 from django.contrib import messages
 
-
 def about_me(request):
-    about = About.objects.all().order_by('-updated_on').first()
-
     if request.method == "POST":
         collaborate_form = CollaborateForm(data=request.POST)
         if collaborate_form.is_valid():
             collaborate_form.save()
-            messages.success(request, "Thanks for reaching out! Your message has been sent.")
-            return redirect("about")  # Reloads the page (prevents resubmitting)
-    else:
-        collaborate_form = CollaborateForm()
+            messages.add_message(
+                request,
+                messages.SUCCESS,
+                "Collaboration request received! I endeavour to respond within 2 working days."
+            )
+
+    about = About.objects.all().order_by('-updated_on').first()
+    collaborate_form = CollaborateForm()
 
     return render(
         request,
